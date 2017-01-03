b02ac00f923b94a7454e27f274eaff733148183d
liyang <18678395180@163.com>
2017-01-03 11:04:59 +0800

    CustomMultipartResolver

--- src/main/java/com/teardowall/core/fileupload/CustomMultipartResolver.java
+++ src/main/java/com/teardowall/core/fileupload/CustomMultipartResolver.java
@@ -2,17 +2,17 @@ package com.teardowall.core.fileupload;
 
 import java.util.List;
 
-import javax.servlet.ServletContext;
+//import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.apache.commons.fileupload.FileItem;
 import org.apache.commons.fileupload.FileItemFactory;
 import org.apache.commons.fileupload.FileUpload;
-import org.apache.commons.fileupload.FileUploadBase;
-import org.apache.commons.fileupload.FileUploadException;
+//import org.apache.commons.fileupload.FileUploadBase;
+//import org.apache.commons.fileupload.FileUploadException;
 import org.apache.commons.fileupload.servlet.ServletFileUpload;
-import org.springframework.web.multipart.MaxUploadSizeExceededException;
+//import org.springframework.web.multipart.MaxUploadSizeExceededException;
 import org.springframework.web.multipart.MultipartException;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
 import org.springframework.web.multipart.commons.CommonsMultipartResolver;
@@ -44,14 +44,14 @@ public class CustomMultipartResolver extends CommonsMultipartResolver {
     // 设置监听器
     MyProgressListener progressListener = new MyProgressListener(request.getSession());
     fileUpload.setProgressListener(progressListener);
-    try {
-      List<FileItem> fileItems = ((ServletFileUpload) fileUpload).parseRequest(request);
+    //try {
+      List<FileItem> fileItems = null;//((ServletFileUpload) fileUpload).parseRequest(request);
       return parseFileItems(fileItems, encoding);
-    } catch (FileUploadBase.SizeLimitExceededException ex) {
-      throw new MaxUploadSizeExceededException(fileUpload.getSizeMax(), ex);
-    } catch (FileUploadException ex) {
-      throw new MultipartException("Could not parse multipart servlet request", ex);
-    }
+//    } catch (FileUploadBase.SizeLimitExceededException ex) {
+//      throw new MaxUploadSizeExceededException(fileUpload.getSizeMax(), ex);
+//    } catch (FileUploadException ex) {
+//      throw new MultipartException("Could not parse multipart servlet request", ex);
+//    }
   }
 
 }
